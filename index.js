8da5f37c053601e8a43a9b980ada8914f6378330
presh <adeyinkaprecious04@gmail.com>
2023-09-25 11:45:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const hourEl = document.querySelector(".hours");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".seconds");
+
+function updateClock() {
+    const currentDate = new Date();
+    setTimeout(updateClock, 1000);
+    const hour = currentDate.getHours();
+    const minute = currentDate.getMinutes();
+    const second = currentDate.getSeconds();
+    const hourDeg = (hour / 12) * 360;
+   
+    hourEl.style.transform = `rotate(${hourDeg}deg)`;
+    const minuteDeg = (minute / 60) * 360;
+    minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
+    const secondDeg = (second / 60) * 360;
+    secondEl.style.transform = `rotate(${secondDeg}deg)`;
+}
+
+ updateClock();
+
+//setInterval(updateClock, 1000)
